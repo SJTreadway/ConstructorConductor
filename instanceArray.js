73ffba311d67d62bf4fc827e39215f09d4eb1c00
SJTreadway <treadway08@gmail.com>
2015-10-28 14:49:03 -0500

    completes instanceArray project

--- instanceArray.js
+++ instanceArray.js
@@ -14,18 +14,23 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
+var users = [];
 
 
 //Now create and push into your users array 3 separate instances of User using the data from above in that exact order
 
   //code here
+var user1 = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+var user2 = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+var user3 = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
+users.push(user1, user2, user3);
 
-console.log('Tyler\'s information is ');
+console.log('Tyler\'s information is username: ' + user1.name + ', email: ' + user1.email + ', pw: ' + user1.pw);
 //Console.log all of Tylers information
 
   //code here
 
-console.log('Lenny\'s information is ');
+console.log('Lenny\'s information is username: ' + user3.name + ', email: ' + user3.email + ', pw: ' + user3.pw);
 //Now console.log all of Lennys information
 
   //code here
@@ -34,8 +39,18 @@ console.log('Lenny\'s information is ');
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  var user4 = new User('Steven', 'treadway08@gmail.com', '@ngularn0de');
+  users.push(user4);
 
-console.log('All my users names are ');
+function looper(arr){
+  var arr2 = [];
+  for(var i = 0; i < arr.length; i++){
+    arr2.push(arr[i].name);
+  }
+  return arr2;
+}
+
+console.log('All my users names are ' + looper(users));
 //Now loop through your users Array and console.log every users name. 
 
   //code here
