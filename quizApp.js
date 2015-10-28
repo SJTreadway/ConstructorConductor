80369f7e9bc7da3313d449c15cf6f72e46b77c94
SJTreadway <treadway08@gmail.com>
2015-10-28 15:33:48 -0500

    80 percent completes project

--- quizApp.js
+++ quizApp.js
@@ -6,27 +6,42 @@
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
   //code here
+  function User(name, email, password, totalScore){
+  	this.name = name;
+  	this.email = email;
+  	this.password = password;
+  	this.totalScore = totalScore;
+  }
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
   //code here
-
+  function Question(title, answersArray, rightAnswer, difficulty){
+  	this.title = title;
+  	this.answersArray = answersArray;
+  	this.rightAnswer = rightAnswer;
+  	this.difficulty = difficulty;
+  }
 
 //Create a users Array which is going to hold all of our users.
 
   //code here
-
+  var users = [];
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
   //code here
-
-
+var mike = new User('Mike', 'mike@gmail.com', '@ngularn0de');
+var jason = new User('Jason', 'jason@gmail.com', 'm0ng0d8');
+var steve = new User('Steve', 'steve@gmail.com', 'ru8y0nr@i1s');
+users.push(mike, jason, steve);
 //Create a questions Array which is going to hold all of our questions
 
   //code here
-
+var questions = [];
+var answers = ['F', 'F', 'T'];
+var rightAnswer = [];
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
 //title: 'T/F: Inheritance is achieved in JavaScript through Prototypes?'
@@ -35,11 +50,23 @@
 //Fill in the rest of the required data as you see appropriate.
 
   //code here
-
+  var q1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?', answers, 'T', 1);
+  var q2 = new Question('T/F: JavaScript is just a scripting version of Java', answers, 'F', 3);
+  var q3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", answers, 'T', 2);
 
 //Now push all of your instances of Question into the questions Array
 
   //code here
+  questions.push(q1, q2, q3);
+
+  function questionLooper(qNum, answerArr){
+  	for(var i in qNum){
+  		if( qNum[i] === answerArr[i]){
+  			rightAnswer.push(answerArr);
+  		}
+  	}
+  	return rightAnswer;
+  }
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
